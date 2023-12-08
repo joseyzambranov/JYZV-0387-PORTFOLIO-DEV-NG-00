0fb9005dc78b5064256899bb644e83d17d1b7fe3
joseyzambranov <joseyzambranovpe@gmail.com>
2023-12-08 14:38:11 -0500

    added pdf

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-about',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
+  constructor(private location: Location) { }
 
+  getPdfUrl(): string {
+    const language = this.location.path().split('/')[1]; // Obtén el idioma de la URL
+    const pdfFileName = `portfolio-${language}.pdf`; // Asegúrate de tener archivos PDF con nombres correspondientes
+
+    return `./assets/pdf/${pdfFileName}`;
+  }
 }
